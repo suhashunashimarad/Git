864548b3c292d0be39b02e42a3a944971ca5bb58
Suhas Hunashimarad <31779720+suhashunashimarad@users.noreply.github.com>
2021-08-26 01:13:08 +0530

    Update App.java

--- GitDemo/src/com/git/App.java
+++ GitDemo/src/com/git/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("GitHub..");
 		
-		System.out.println("Changes");
+		System.out.println("Dev1 Changes");
 		
 	}
 
